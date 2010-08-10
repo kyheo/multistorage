3b6180c01b3810a96272dc3a1ac1561f6c6b6492
Martin Marrese <marrese@gmail.com>
2010-08-09 23:52:34 -0300

    Added lib package and resmanager (handles pymongo connections)

---
+++ lib/resmanager.py
@@ -0,0 +1,20 @@
+import pymongo
+
+class ResManager(object):
+
+    _INS = None 
+
+    @classmethod
+    def get(cls, site, col):
+        if ResManager._INS is None:
+            ResManager._INS = ResManager(site, col)
+        return ResManager._INS.col
+
+    @classmethod
+    def end(cls):
+        ResManager._INS.con.end_request()
+
+    def __init__(self, site, col):
+        self.con = pymongo.Connection()
+        self.db  = self.con[site]
+        self.col = self.db[col]
