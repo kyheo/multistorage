3e3b1d410ed489e1ca588dde6d3541ffbbc4f5a8
Martin Marrese <marrese@gmail.com>
2011-05-21 13:04:24 -0300

    Starter nscript updates

--- multistorage/multistorage.py
+++ multistorage/multistorage.py
@@ -7,23 +7,28 @@ from tornado.options import define, options
 
 import logging
 
-from lib.routes_handlers import *
+from lib import handlers
 
 
-routes = \
-   [
-    (r"/([0-9a-zA-Z]+)/([0-9a-zA-Z]+)/", CollectionHandler),
-    (r"/([0-9a-zA-Z]+)/([0-9a-zA-Z]+)/([0-9a-zA-Z]+)", ItemHandler),
-   ]
+routes = [(r"/([0-9a-zA-Z]+)/([0-9a-zA-Z]+)/", handlers.CollectionHandler),
+          (r"/([0-9a-zA-Z]+)/([0-9a-zA-Z]+)/([0-9a-zA-Z]+)", handlers.ItemHandler)]
+
+
+define('port'   , type=int , default=8000 , help='run on the given port')
+define('debug'  , type=bool, default=False, help='enable debug mode'    )
+define('no-gzip', type=bool, default=True , help='disables gzip'          )
 
 
 def main():
+    """Starts application server"""
     tornado.options.parse_command_line()
-    application = tornado.web.Application(routes)
+    logging.info('Listen on port %d' % (options.port))
+    settings = {'gzip': True, 'debug': options.debug}
+    application = tornado.web.Application(routes, **settings)
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
 
+
 if __name__ == "__main__":
-    define("port", default=8888, help="run on the given port", type=int)
     main()
